bab8197f249f3f57928fa0e7267350d961a6dc36
fu-mix <fuminaka@gmail.com>
2021-07-06 00:44:26 +0900

    fix:sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -38,7 +38,7 @@ module.exports = {
         {
           type: 'category',
           label: 'Nested item',
-          items: ['hellow'],
+          items: ['guide/ellow'],
         }
       ]
     }
